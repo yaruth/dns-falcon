1a49e36efb56850a98d65cdf11cad3582bc2a4e6
yaruth <huy_94@hotmail.de>
2022-06-15 21:08:22 +0000

    added dnskey to algorollover

--- setup.py
+++ setup.py
@@ -168,13 +168,19 @@ def add_test_setup(parent: dns.name.Name, ns_ip4_set: Set[str], ns_ip6_set: Set[
             
             # new RRSIGs
             name = dns.name.Name(('new_rrsig-' + algorithm + ('3' if nsec == 3 else ''),)) + parent
-            print("huhuhuhhuhuhu:")
-            print(name)
             add_zone(name, algorithm, nsec)
             delegate_auth(name, parent, ns_ip4_set, ns_ip6_set)            
-            print("debugggerhuybui:" + "add-zone-key" + "" + name.to_text() + "debugende")
             auth("add-zone-key", name.to_text(), "KSK", "active", "unpublished", "falcon")
-
+            
+            # new DNSKEY
+            print("WASSAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP")
+            name = dns.name.Name(('new_dnskey-' + algorithm + ('3' if nsec == 3 else ''),)) + parent
+            add_zone(name, algorithm, nsec)
+            delegate_auth(name, parent, ns_ip4_set, ns_ip6_set)
+            out = auth("add-zone-key", name.to_text(), "KSK", "active", "unpublished", "falcon")
+            id = out.splitlines()[-1]
+            print(id)
+            auth("publish-zone-key", name.to_text(), id)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
